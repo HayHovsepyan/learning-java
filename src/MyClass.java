0794d6b2c0968193cb966c4f319b2fd620296521
HayHovsepyan <hay.hovsepyan@gmail.com>
2019-01-07 07:28:39 -0800

    App_426

---
+++ src/MyClass.java
@@ -0,0 +1,16 @@
+class MyClass  {
+    public static void main(String[ ] args) {
+        int [][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int k = 2;
+        int count = 0;
+        
+        for (int i = 0; i < a.length; i++){
+            for (int j = 0; i > j; j++){
+                if (a[i][j] % k == 0)
+            
+            count = count + 1;
+            }
+        }
+        System.out.println(count);
+    }
+}
